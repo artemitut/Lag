0f8b5ed54e4c7469bdf23aa49700a8307b4454db
artemitut <92166551+artemitut@users.noreply.github.com>
2022-02-16 16:45:41 +0200

    second

---
+++ index.js
@@ -0,0 +1,20 @@
+let i = 1
+while(i>0){
+    i+=10000
+    console.log(i)
+}
+let a = 1
+while(i>0){
+    a+=10000
+    console.log(i)
+}
+let b = 1
+while(i>0){
+    b+=10000
+    console.log(i)
+}
+let c = 1
+while(i>0){
+    c+=10000
+    console.log(i)
+}
