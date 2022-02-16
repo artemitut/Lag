9eaccef16a575a1ba39b462ff286e81ad97bcd1a
artemitut <92166551+artemitut@users.noreply.github.com>
2022-02-16 17:36:57 +0200

    3

--- index.js
+++ index.js
@@ -1,20 +1,71 @@
-let i = 1
-while(i>0){
-    i+=10000
+// let i = 1
+// while(i>0){
+//     i++
+//     console.log(i)
+// }
+// let a = 1
+// while(a>0){
+//     a+=10000
+//     console.log(a)
+// }
+// let b = 1
+// while(i>0){
+//     b+=10000
+//     console.log(b)
+// }
+// let c = 1
+// while(i>0){
+//     c+=10000
+//     console.log(c)
+// }
+// let z = 1
+// while(i>0){
+//     z+=10000
+//     console.log(i)
+// }
+// let x = 1
+// while(i>0){
+//     x+=10000
+//     console.log(i)
+// }
+// let v = 1
+// while(i>0){
+//     v+=10000
+//     console.log(i)
+// }
+// let n = 100
+// while(i>0){
+//     n+=n
+//     console.log(i)
+// }
+
+let i = 10000000
+for (x=1;x>0;x++) {
+    i*=i
     console.log(i)
 }
-let a = 1
-while(i>0){
-    a+=10000
-    console.log(i)
+let a = 10000000
+for (x=1;x>0;x++) {
+    a*=a
+    console.log(a)
 }
-let b = 1
-while(i>0){
-    b+=10000
-    console.log(i)
+let s = 10000000
+for (x=1;x>0;x++) {
+    s*=s
+    console.log(s)
 }
-let c = 1
-while(i>0){
-    c+=10000
-    console.log(i)
+let z = 10000000
+for (x=1;x>0;x++) {
+    z*=z
+    console.log(z)
 }
+// let x = 10000000
+// for (x=1;x>0;x++) {
+//     x*=x
+//     console.log(x)
+// }
+// let v = 10000000
+// for (x=1;x>0;x++) {
+//     v*=v
+//     console.log(v)
+// }
